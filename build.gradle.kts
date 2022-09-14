c9f32cd2d102199f89570c23f345db27a9882bcb
stheren <silvain.therene@gmail.com>
2022-09-14 16:13:00 +0200

    [Feat] Add Socket dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.ir.backend.js.compile
 
+val iosocketVersion: String by project
+
 plugins {
     kotlin("jvm")
     id("com.github.johnrengelman.shadow")
@@ -17,6 +19,9 @@ dependencies {
     implementation(kotlin("stdlib-jdk8"))
 
     implementation("de.jensd:fontawesomefx:8.9")
+    implementation ("io.socket:socket.io-client:${iosocketVersion}") {
+        exclude(group = "org.json", module = "json")
+    }
 
     testImplementation(kotlin("test"))
 }
